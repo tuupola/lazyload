3b6e1d90f7571d8e3ca0b9093ca43b6b3f1238ba
Mika Tuupola <tuupola@appelsiini.net>
2007-09-02 18:01:51 +0000

    Rename directory.

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -1,38 +1,37 @@
 (function($) {
     
-    $.fn.belowthefold = function(options) {
+    $.fn.lazyload = function(options) {
         var settings = {
-            adjust     : 0
+            threshold : 0
         };
-
+        
         if(options) {
             $.extend(settings, options);
         };
 
         return this.each(function() {
             var self = this;
-
+            
             $(self).attr("original", $(self).attr("src"));
-            if ($.belowthefold(self)) {
+            if ($.belowthefold(self, settings)) {
                 $(self).attr("src", "");                
             }
 
             $(document).bind("scroll", function(event) {
-                if ($.abovethefold(self)) {
+                if (!$(self).attr("src") && $.abovethefold(self, settings)) {
                     $(self).attr("src", $(self).attr("original"));                
                 };
             });
-
         });
     };
 
-    $.belowthefold = function(element, options) {
+    $.belowthefold = function(element, settings) {
         var fold = $(window).height() + $(window).scrollTop();
-        return fold <= $(element).offset().top;
+        return fold <= $(element).offset().top - settings.threshold;
     }
 
-    $.abovethefold = function(element, options) {
-        return !$.belowthefold(element, options);
+    $.abovethefold = function(element, settings) {
+        return !$.belowthefold(element, settings);
     }
     
 })(jQuery);
