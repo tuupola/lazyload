d6601b56e377bc62a185bb36fe8238879a411a72
Mika Tuupola <tuupola@appelsiini.net>
2008-11-14 11:41:51 +0200

    Fix error when serving application/xhtml+xml.
    Bug reported by Chris Pappas <chrispappas12__at__gmail.com>

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -71,7 +71,7 @@
             /* When appear is triggered load original image. */
             $(self).one("appear", function() {
                 if (!this.loaded) {
-                    $("<img>")
+                    $("<img />")
                         .attr("src", $(self).attr("original"))
                         .bind("load", function() {
                             $(self)
