ff59ce03c59a469654e79da4924256231750b22a
Mika Tuupola <tuupola@appelsiini.net>
2013-11-16 20:49:02 +0200

    Use data:uri placeholder only with image elements.

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -90,7 +90,9 @@
 
             /* If no src attribute given use data:uri. */
             if ($self.attr("src") === undefined || $self.attr("src") === false) {
-                $self.attr("src", settings.placeholder);
+                if ($self.is("img")) {
+                    $self.attr("src", settings.placeholder);
+                }
             }
 
             /* When appear is triggered load original image. */
