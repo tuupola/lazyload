dd3dca26cd89d6e8ee48482d2347603275995493
Vladimir Sobolev <v.sobolev@gmail.com>
2014-01-19 17:06:36 -0500

    Change placeholder image format
    png changed to gif (35 bytes only)

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -29,7 +29,7 @@
             skip_invisible  : true,
             appear          : null,
             load            : null,
-            placeholder     : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
+            placeholder     : "data:image/gif;base64,R0lGODdhAQABAPAAAMPDwwAAACwAAAAAAQABAAACAkQBADs="
         };
 
         function update() {
