61e476ace585ffa546dbdf5d99b4fbc8ba29f024
Alexey Kozlov <ozon1234@gmail.com>
2014-04-25 18:23:05 +0400

    Using $().data() instead of $().attr('data-' + ...)

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -103,7 +103,7 @@
                     $("<img />")
                         .bind("load", function() {
 
-                            var original = $self.attr("data-" + settings.data_attribute);
+                            var original = $self.data(settings.data_attribute);
                             $self.hide();
                             if ($self.is("img")) {
                                 $self.attr("src", original);
@@ -125,7 +125,7 @@
                                 settings.load.call(self, elements_left, settings);
                             }
                         })
-                        .attr("src", $self.attr("data-" + settings.data_attribute));
+                        .attr("src", $self.data(settings.data_attribute));
                 }
             });
 
