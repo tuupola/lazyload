d7e845bca13eab191d6e0f5c24f4e5b187a41d1b
Travis Dahl <travdahl@gmail.com>
2014-09-03 15:34:02 -0700

    Add Error callback for when images requests fail

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -29,6 +29,7 @@
             skip_invisible  : true,
             appear          : null,
             load            : null,
+            error           : null,
             placeholder     : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
         };
 
@@ -125,6 +126,19 @@
                             if (settings.load) {
                                 var elements_left = elements.length;
                                 settings.load.call(self, elements_left, settings);
+                            }
+                        }).bind("error", function() {
+                            self.error = true;
+
+                            /* Remove image from array so it is not looped next time. */
+                            var temp = $.grep(elements, function(element) {
+                                return !element.error;
+                            });
+                            elements = $(temp);
+
+                            if (settings.error) {
+                                var elements_left = elements.length;
+                                settings.error.call(self, elements_left, settings);
                             }
                         })
                         .attr("src", $self.attr("data-" + settings.data_attribute));
