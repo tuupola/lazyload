144ab2e72372bc9ef3c92a70f513aedbbe125827
Vladimir Rudenko <rudenko@farpost.com>
2016-01-12 14:58:07 +1000

    srcset support

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -132,7 +132,10 @@
                                 settings.load.call(self, elements_left, settings);
                             }
                         })
-                        .attr("src", $self.attr("data-" + settings.data_attribute));
+                        .attr({
+                            "src": $self.attr("data-" + settings.data_attribute),
+                            "srcset": $self.attr("data-" + settings.data_srcset) || ""
+                        });
                 }
             });
 
