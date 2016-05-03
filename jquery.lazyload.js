945b0a0ff3dfed35e43921ba5136bef639123505
Johnny Shankman <john.shankman@refinery29.com>
2016-05-03 13:01:44 -0400

    Creates quick_swap setting

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -27,6 +27,7 @@
             container       : window,
             data_attribute  : "original",
             skip_invisible  : false,
+            quick_swap      : false,
             appear          : null,
             load            : null,
             placeholder     : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
